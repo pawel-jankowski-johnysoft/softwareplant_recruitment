9fb2dfbf696833a7e2769c9b4b76c319f5d124dc
pawel-jankowski-johnysoft <47305006+pawel-jankowski-johnysoft@users.noreply.github.com>
2020-04-02 20:53:56 +0200

    Add way to create and add report + prepare contract to communicate with external service (#4)

--- src/main/java/com/johnysoft/softwareplant_recruitment/report/generate/ReportGenerator.java
+++ src/main/java/com/johnysoft/softwareplant_recruitment/report/generate/ReportGenerator.java
@@ -1,10 +1,36 @@
 package com.johnysoft.softwareplant_recruitment.report.generate;
 
+import com.johnysoft.softwareplant_recruitment.report.generate.swapi.SwapiDataProvider;
+import com.johnysoft.softwareplant_recruitment.report.generate.swapi.SwapiSearchParams;
+import com.johnysoft.softwareplant_recruitment.report.generate.swapi.model.SwapiDataModel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Component;
 
+import static lombok.AccessLevel.PRIVATE;
+
 @Component
+@FieldDefaults(makeFinal = true, level = PRIVATE)
+@RequiredArgsConstructor
 class ReportGenerator {
+    SwapiDataProvider swapiDataProvider;
+    ReportCreator reportCreator;
 
     public void generateReport(Long reportId, GenerateReportQueryCriteria criteria) {
+        final SwapiDataModel swapiData = getSwapiData(criteria);
+        buildReport(reportId, criteria, swapiData);
+    }
+
+    private SwapiDataModel getSwapiData(GenerateReportQueryCriteria criteria) {
+        return swapiDataProvider.getReportData(SwapiSearchParams.builder()
+                .characterPhrase(criteria.getQueryCriteriaCharacterPhrase())
+                .planetName(criteria.getQueryCriteriaPlanetName()).build());
+    }
+
+    private void buildReport(Long reportId, GenerateReportQueryCriteria criteria, SwapiDataModel swapiData) {
+        reportCreator.withId(reportId)
+                .queryCriteria(criteria.getQueryCriteriaCharacterPhrase(), criteria.getQueryCriteriaPlanetName())
+                .records(swapiData)
+                .build();
     }
 }
