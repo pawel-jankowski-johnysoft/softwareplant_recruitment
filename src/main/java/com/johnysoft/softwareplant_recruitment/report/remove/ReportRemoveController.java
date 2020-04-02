d40f6479a97415ea28874a3892e8ab2853864d66
pawel-jankowski-johnysoft <47305006+pawel-jankowski-johnysoft@users.noreply.github.com>
2020-04-02 21:18:24 +0200

    small code improvements (#5)

--- src/main/java/com/johnysoft/softwareplant_recruitment/report/remove/ReportRemoveController.java
+++ src/main/java/com/johnysoft/softwareplant_recruitment/report/remove/ReportRemoveController.java
@@ -1,23 +1,27 @@
 package com.johnysoft.softwareplant_recruitment.report.remove;
 
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.johnysoft.softwareplant_recruitment.report.remove.ReportRemoveController.DELETE_REPORT_URL;
+import static lombok.AccessLevel.PRIVATE;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
-@RequestMapping(ReportRemoveController.DELETE_REPORT_URL)
+@RequestMapping(DELETE_REPORT_URL)
 @RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = PRIVATE)
 class ReportRemoveController {
 
     static final String DELETE_REPORT_BY_ID_URL = "/{reportId}";
     static final String DELETE_REPORT_URL = "/report";
 
-    private final ReportRemover reportRemover;
+    ReportRemover reportRemover;
 
     @DeleteMapping
     @ResponseStatus(NO_CONTENT)
--- src/test/java/com/johnysoft/softwareplant_recruitment/AbstractDocumentationTest.java
+++ src/test/java/com/johnysoft/softwareplant_recruitment/AbstractDocumentationTest.java
@@ -2,14 +2,12 @@ package com.johnysoft.softwareplant_recruitment;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@AutoConfigureMockMvc
 public abstract class AbstractDocumentationTest {
     @LocalServerPort
     private int serverPort;
