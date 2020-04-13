efda9c295361e665db70c9af78fda2aef2889299
Johnthebest <pawel.jankowski.johnysoft@gmail.com>
2020-04-13 13:07:10 +0200

    logging as test user at startup

--- src/front/report-front/src/app/app.component.ts
+++ src/front/report-front/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {flatMap} from "rxjs/operators";
+
+const USERNAME = 'test';
+const PASSWORD = 'test';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'report-front';
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    const formData = new FormData();
+    formData.append('username', USERNAME);
+    formData.append('password', PASSWORD);
+
+    const subscription = this.http.post('/login', formData)
+      .pipe(flatMap(_ => this.router.navigate(['/report/list'])))
+      .subscribe(_ => subscription.unsubscribe());
+  }
+
+
 }
