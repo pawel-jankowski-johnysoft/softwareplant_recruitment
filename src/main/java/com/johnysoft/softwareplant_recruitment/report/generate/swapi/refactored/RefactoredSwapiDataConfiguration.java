ff7fd66c91c0f8bb85752f5b8e3904b5463aaf38
Johnthebest <pawel.jankowski.johnysoft@gmail.com>
2020-04-15 20:24:33 +0200

    step 3 - use new mocked implementation in test (FAILED!!!)

---
+++ src/main/java/com/johnysoft/softwareplant_recruitment/report/generate/swapi/refactored/RefactoredSwapiDataConfiguration.java
@@ -0,0 +1,21 @@
+package com.johnysoft.softwareplant_recruitment.report.generate.swapi.refactored;
+
+import com.johnysoft.softwareplant_recruitment.report.generate.swapi.SwapiDataProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ConditionalOnProperty(prefix = "swapi", value = "base_url")
+class RefactoredSwapiDataConfiguration {
+
+    private static final String SWAPI_BASE_URL = "${swapi.base_url}";
+
+    @Primary
+    @Bean
+    public SwapiDataProvider swapiDataProvider(@Value(SWAPI_BASE_URL) String swapiBaseUrl) {
+        return new RefactoredSwapiDataProvider();
+    }
+}
